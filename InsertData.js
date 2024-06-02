32ea12ec370ed5522a98a0a21fa483d562e8f68f
Xm3Ga <adripbahamont@gmail.com>
2024-06-02 18:54:13 +0200

    InsertData

--- InsertData.js
+++ InsertData.js
@@ -1,57 +1,55 @@
 const mongoose = require("mongoose");
-const { getDb } = require("./modules/mongodb/mongodb.util");
 
 // Importar los modelos y los datos
-const desarrolladora = require("./modules/desarrolladora/desarrolladora.model");
+const Desarrolladora = require("./modules/desarrolladora/desarrolladora.model");
 const desarrolladoraData = require("./data/DatosDesarrolladora");
 
-const editorial = require("./modules/editorial/editorial.model");
+const Editorial = require("./modules/editorial/editorial.model");
 const editorialData = require("./data/DatosEditorial");
 
-const editorialsmesa = require("./modules/editorialsmesa/editorialsmesa.model");
+const Editorialsmesa = require("./modules/editorialsmesa/editorialsmesa.model");
 const editorialsmesaData = require("./data/DatosEditorialsmesa");
 
-const empleados = require("./modules/empleados/empleados.model");
+const Empleados = require("./modules/empleados/empleados.model");
 const empleadosData = require("./data/DatosEmpleados");
 
-const encargos = require("./modules/encargos/encargos.model");
+const Encargos = require("./modules/encargos/encargos.model");
 const encargosData = require("./data/DatosEncargos");
 
-const generos = require("./modules/generos/generos.model");
+const Generos = require("./modules/generos/generos.model");
 const generosData = require("./data/DatosGenero");
 
-const generosmesa = require("./modules/generosmesa/generosmesa.model");
+const Generosmesa = require("./modules/generosmesa/generosmesa.model");
 const generosmesaData = require("./data/DatosGenerosmesa");
 
-const miembros = require("./modules/miembros/miembros.model");
+const Miembros = require("./modules/miembros/miembros.model");
 const miembrosData = require("./data/DatosMiembros");
 
-const sagas = require("./modules/sagas/sagas.model");
+const Sagas = require("./modules/sagas/sagas.model");
 const sagasData = require("./data/DatosSagas");
 
-const stockConduccion = require("./modules/stockConduccion/stockconduccion.model");
+const StockConduccion = require("./modules/stockConduccion/stockconduccion.model");
 const stockConduccionData = require("./data/DatosStockconduccion");
 
-const stockIndies = require("./modules/stockIndies/stockIndies.model");
-const stockIndiesData = require("./data/DatosStock");
+const StockIndies = require("./modules/stockIndies/stockIndies.model");
 
-const stocksmesa = require("./modules/stocksmesa/stocksmesa.model");
+const Stocksmesa = require("./modules/stocksmesa/stocksmesa.model");
 const stocksmesaData = require("./data/DatosStocksmesa");
 
 // Comprobar si hay datos
 async function isDatabaseEmpty() {
-    const desarrolladoraCount = await desarrolladora.countDocuments();
-    const editorialCount = await editorial.countDocuments();
-    const editorialsmesaCount = await editorialsmesa.countDocuments();
-    const empleadosCount = await empleados.countDocuments();
-    const encargosCount = await encargos.countDocuments();
-    const generosCount = await generos.countDocuments();
-    const generosmesaCount = await generosmesa.countDocuments();
-    const miembrosCount = await miembros.countDocuments();
-    const sagasCount = await sagas.countDocuments();
-    const stockConduccionCount = await stockConduccion.countDocuments();
-    const stockIndiesCount = await stockIndies.countDocuments();
-    const stocksmesaCount = await stocksmesa.countDocuments();
+    const desarrolladoraCount = await Desarrolladora.countDocuments();
+    const editorialCount = await Editorial.countDocuments();
+    const editorialsmesaCount = await Editorialsmesa.countDocuments();
+    const empleadosCount = await Empleados.countDocuments();
+    const encargosCount = await Encargos.countDocuments();
+    const generosCount = await Generos.countDocuments();
+    const generosmesaCount = await Generosmesa.countDocuments();
+    const miembrosCount = await Miembros.countDocuments();
+    const sagasCount = await Sagas.countDocuments();
+    const stockConduccionCount = await StockConduccion.countDocuments();
+    const stockIndiesCount = await StockIndies.countDocuments();
+    const stocksmesaCount = await Stocksmesa.countDocuments();
 
     return (
         desarrolladoraCount === 0 &&
@@ -71,18 +69,50 @@ async function isDatabaseEmpty() {
 
 // Insertar datos
 async function insertData() {
-    await desarrolladora.insertMany(desarrolladoraData);
-    await editorial.insertMany(editorialData);
-    await editorialsmesa.insertMany(editorialsmesaData);
-    await empleados.insertMany(empleadosData);
-    await encargos.insertMany(encargosData);
-    await generos.insertMany(generosData);
-    await generosmesa.insertMany(generosmesaData);
-    await miembros.insertMany(miembrosData);
-    await sagas.insertMany(sagasData);
-    await stockConduccion.insertMany(stockConduccionData);
-    await stockIndies.insertMany(stockIndiesData);
-    await stocksmesa.insertMany(stocksmesaData);
+    try {
+        await Desarrolladora.insertMany(desarrolladoraData);
+        await Editorial.insertMany(editorialData);
+        await Editorialsmesa.insertMany(editorialsmesaData);
+        await Empleados.insertMany(empleadosData);
+        await Encargos.insertMany(encargosData);
+        await Generos.insertMany(generosData);
+        await Generosmesa.insertMany(generosmesaData);
+        await Miembros.insertMany(miembrosData);
+        await Sagas.insertMany(sagasData);
+        await StockConduccion.insertMany(stockConduccionData);
+        await Stocksmesa.insertMany(stocksmesaData);
+
+        // Obtener los ObjectId insertados
+        const generosDocs = await Generos.find();
+        const desarrolladoraDocs = await Desarrolladora.find();
+
+        // Mapear los nombres de generos y desarrolladoras a sus respectivos ObjectId
+        const generosMap = {};
+        generosDocs.forEach(doc => {
+            generosMap[doc.Genero] = doc._id;
+        });
+
+        const desarrolladoraMap = {};
+        desarrolladoraDocs.forEach(doc => {
+            desarrolladoraMap[doc.Desarrolladora] = doc._id;
+        });
+
+        // Actualizar datos de stockIndies con ObjectIds reales
+        const stockIndiesDataWithIds = [
+            new StockIndies({ ID: 1, Nombre: 'Hollow Knight', stock: 19, Genero: generosMap['Metroidvania'], Desarrollador: desarrolladoraMap['Team Cherry'], Precio: 10 }),
+            new StockIndies({ ID: 2, Nombre: 'Dredge', stock: 8, Genero: generosMap['Pesca'], Desarrollador: desarrolladoraMap['Black Salt Games'], Precio: 5 }),
+            new StockIndies({ ID: 3, Nombre: 'Whos Lila', stock: 50, Genero: generosMap['Surrealismo'], Desarrollador: desarrolladoraMap['Garage Heathen'], Precio: 10 }),
+            new StockIndies({ ID: 4, Nombre: 'Cruelty Squad', stock: 27, Genero: generosMap['Sátira'], Desarrollador: desarrolladoraMap['Consumer Softproducts'], Precio: 15 }),
+            new StockIndies({ ID: 5, Nombre: 'Return of the Obra Dinn', stock: 6, Genero: generosMap['Misterio'], Desarrollador: desarrolladoraMap['Lucas Pope'], Precio: 15 })
+        ];
+
+        // Insertar datos en stockIndies
+        await StockIndies.insertMany(stockIndiesDataWithIds);
+
+        console.log("Datos insertados correctamente.");
+    } catch (error) {
+        console.error("Error al insertar los datos:", error);
+    }
 }
 
 // Insertar datos si no está vacía
