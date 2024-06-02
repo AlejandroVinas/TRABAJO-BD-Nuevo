7b75b0fc27a901d3a17619ada4f3d92272b184c0
miguelgomez75 <miguel.gomez@alumnos.uneatlantico.es>
2024-06-02 12:12:11 +0200

    Archivo encargado de insertar la data si la base de datos está vacía

--- InsertData.js
+++ InsertData.js
@@ -1,6 +1,42 @@
 const mongoose = require("mongoose");
 
 // Importar los modelos y los datos
+const desarrolladora = require("./modules/desarrolladora/desarroladora.model");
+const desarrolladoraData = require("./data/DatosDesarrolladora");
+
+const editorial = require("./modules/editorial/editorial.model");
+const editorialData = require("./data/DatosEditorial");
+
+const editorialsmesa = require("./modules/editorialmesa/editorialmesa.model");
+const editorialmesaData = require("./data/DatosEditorialsmesa");
+
+const empleados = require("./modules/empleados/empleados.model");
+const empleadosData = require("./data/DatosEmpleados");
+
+const encargos = require("./modules/encargos/encargos.model");
+const encargosData = require("./data/DatosEncargos");
+
+const generos = require("./modules/generos/generos.model");
+const generosData = require("./data/DatosGenero");
+
+const generosmesa = require("./modules/generosmesa/generosmesa.model");
+const generosmesaData = require("./data/DatosGenerosmesa");
+
+const miembros = require("./modules/miembros/miembros.model");
+const miembrosData = require("./data/DatosMiembros");
+
+const sagas = require("./modules/sagas/sagas.model");
+const sagasData = require("./data/DatosSagas");
+
+const stockConduccion = require("./modules/stockConduccion/stockConduccion.model");
+const stockConduccionData = require("./data/DatosStockconduccion");
+
+const stockIndies = require("./modules/stockIndies/stockIndies.model");
+const stockIndiesData = require("./data/DatosStock");
+
+const stocksmesa = require("./modules/stocksmesa/stocksmesa.model");
+const stocksmesaData = require("./data/DatosStocksmesa");
+
 
 
 
@@ -18,7 +54,18 @@ async function isDatabaseEmpty() {
 
 // Insertar datos
 async function insertData() {
-
+await desarrolladora.insertMany(desarrolladoraData);
+await editorial.insertMany(editorialData);
+await editorialsmesa.insertMany(editorialmesaData);
+await empleados.insertMany(empleadosData);
+await encargos.insertMany(encargosData);
+await generos.insertMany(generosData);
+await generosmesa.insertMany(generosmesaData);
+await miembros.insertMany(miembrosData);
+await sagas.insertMany(sagasData);
+await stockConduccion.insertMany(stockConduccionData);
+await stockIndies.insertMany(stockIndiesData);
+await stocksmesa.insertMany(stocksmesaData);
 }
 
 // insertar datos si no está vacía
